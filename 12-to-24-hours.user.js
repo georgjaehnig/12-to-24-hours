dca9e6359a76614684dc3373a1a0948a63ae2bc3
Georg Jaehnig <georg@jaehnig.org>
2018-12-23 21:43:05 +0100

    Rewrite with mutation observer.

--- 12-to-24-hours.user.js
+++ 12-to-24-hours.user.js
@@ -3,58 +3,63 @@
 // @description  Replaces 11:45 PM to 23:45 on google.com/flights
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @author       Ge
+// @author       Georg Jähnig
 // @match        https://www.google.com/flights*
 // @grant        none
-// @version 0.1
-// @copyright   2018, Georg Jähnig
-// @author Georg Jähnig
-// @license MIT
-// @include *
-// @run-at document-end
-// @grant none
+// @version      0.1
+// @copyright    2018, Georg Jähnig
+// @author       Georg Jähnig
+// @license      MIT
+// @include      *
+// @run-at       document-end
+// @grant        none
 // ==/UserScript==
 
 (function() {
   'use strict';
 
-  function replace(){
+  function replace(matches) {
 
-    // Iterate over all <span>/
-    var elements = document.getElementsByTagName('span');
-    for(var i=0, len=elements.length; i < len; i++){
+    // Get values.
+    var hours = parseInt(matches[1]);
+    var minutes = parseInt(matches[2]);
+    var AMPM = matches[3];
 
-      var element = elements[i];
+    // Set hour.
+    if (AMPM == "PM" && hours < 12) hours = hours + 12;
+    if (AMPM == "AM" && hours == 12) hours = hours - 12;
 
-      // Check if <span> contains time.
-      var matches = element.innerHTML.match(/^(\d{1,2}):(\d{2}) (AM|PM)$/);
+    var sHours = hours.toString();
+    var sMinutes = minutes.toString();
 
-      if (matches) {
-        // Get values.
-        var hours = parseInt(matches[1]);
-        var minutes = parseInt(matches[2]);
-        var AMPM = matches[3];
+    // Append leading zeros.
+    if (hours < 10) sHours = "0" + sHours;
+    if (minutes < 10) sMinutes = "0" + sMinutes;
 
-        // Set hour.
-        if (AMPM == "PM" && hours < 12) hours = hours + 12;
-        if (AMPM == "AM" && hours == 12) hours = hours - 12;
+    // Replace with 24h format.
+    var time24h = (sHours +':'+sMinutes);
 
-        var sHours = hours.toString();
-        var sMinutes = minutes.toString();
+    return time24h;
+  }
 
-        // Append leading zeros.
-        if (hours < 10) sHours = "0" + sHours;
-        if (minutes < 10) sMinutes = "0" + sMinutes;
-
-        // Replace with 24h format.
-        element.innerHTML = (sHours +':'+sMinutes);
-      }
+  function checkMutation(mutation) {
+    var matches = mutation.target.innerHTML.match(/^(\d{1,2}):(\d{2}) (AM|PM)$/)
+    if (matches) {
+      mutation.target.innerHTML = replace(matches);
     }
   }
 
-  console.log('start');
-  // Wait 5 seconds to let render finish.
-  // TODO: Get triggered again on DOM update.
-  setTimeout(replace,5000);
+  var mutationObserver = new MutationObserver(function(mutations) {
+    mutations.forEach(checkMutation);
+  });
+
+  mutationObserver.observe(document.documentElement, {
+    attributes: true,
+    characterData: true,
+    childList: true,
+    subtree: true,
+    attributeOldValue: true,
+    characterDataOldValue: true
+  });
 
 })();
