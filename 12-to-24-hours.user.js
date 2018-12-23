b118ee004d73fbd4492ffdec59567d2735f74fa9
Georg Jaehnig <georg@jaehnig.org>
2018-12-23 21:35:12 +0100

    Whitespace.

--- 12-to-24-hours.user.js
+++ 12-to-24-hours.user.js
@@ -20,41 +20,41 @@
 
   function replace(){
 
-		// Iterate over all <span>/
+    // Iterate over all <span>/
     var elements = document.getElementsByTagName('span');
     for(var i=0, len=elements.length; i < len; i++){
 
       var element = elements[i];
 
-			// Check if <span> contains time.
-      var matches = element.innerHTML.match(/(\d{1,2}):(\d{2}) (AM|PM)$/);
+      // Check if <span> contains time.
+      var matches = element.innerHTML.match(/^(\d{1,2}):(\d{2}) (AM|PM)$/);
 
       if (matches) {
-				// Get values.
+        // Get values.
         var hours = parseInt(matches[1]);
         var minutes = parseInt(matches[2]);
         var AMPM = matches[3];
 
-				// Set hour.
+        // Set hour.
         if (AMPM == "PM" && hours < 12) hours = hours + 12;
         if (AMPM == "AM" && hours == 12) hours = hours - 12;
 
         var sHours = hours.toString();
         var sMinutes = minutes.toString();
 
-				// Append leading zeros.
+        // Append leading zeros.
         if (hours < 10) sHours = "0" + sHours;
         if (minutes < 10) sMinutes = "0" + sMinutes;
 
-				// Replace with 24h format.
+        // Replace with 24h format.
         element.innerHTML = (sHours +':'+sMinutes);
       }
     }
   }
 
-	console.log('start');
-	// Wait 5 seconds to let render finish.
-	// TODO: Get triggered again on DOM update.
+  console.log('start');
+  // Wait 5 seconds to let render finish.
+  // TODO: Get triggered again on DOM update.
   setTimeout(replace,5000);
 
 })();
